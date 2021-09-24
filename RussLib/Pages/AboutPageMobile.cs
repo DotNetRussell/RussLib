f4846e71e5ebef95128907ac41db784e4917fe78
Anthony Russell <anthonyrussellpublic@gmail.com>
2021-09-24 17:38:03 -0400

    Code Cleanup

--- RussLib/Pages/AboutPageMobile.cs
+++ RussLib/Pages/AboutPageMobile.cs
@@ -1,9 +1,4 @@
 ﻿using BasecodeLibrary.Controls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
